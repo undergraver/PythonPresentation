55a022dfa2c6ec17efad7c5fc98ed42cb25034d8
unknown <ilinca@bra-w12-018.lmsintl.com>
2014-05-13 14:47:13 +0300

    function with multiple return values

--- 01_intro/arguments.py
+++ 01_intro/arguments.py
@@ -15,3 +15,15 @@ print_profile("birlic04", "Adjud")
 print_profile("sandel_car", "Caracal", name = "Sandu")
 print_profile("linda", "Rupea", age = 25)
 print_profile("aurica", "Darste", age = 25, name = "Aurelia")
+
+# return multiple values
+def gpsPosition()
+    return (45.655902, 25.610652, 415) # a tupple
+    
+pos = gpsPosition()
+print pos
+
+lat, long, alt = gpsPosition() # use tupple unpacking to assign each item to a variable
+print lat, long, alt
+
+# another way is to use dictionaries
