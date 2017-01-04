a2f68514d113bf884a414bd7206429da95d6006e
Iulian Serbanoiu <undergraver@gmail.com>
2017-01-04 19:53:40 +0200

    holidays in ro (based on current laws - January 2017)

---
+++ 05_financial/ro_holidays.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import datetime
+
+#
+# NOTE: This doesn't take into account days with variable date that always fall on Sunday (Easter or Pentecost) - next Monday after those is also holiday
+#
+
+daysOff = [
+[1,1], # 1st of January
+[2,1], # 2nd of January
+[24,1], #24th of January
+[1,5],
+[1,6],
+[15,8],
+[30,11],
+[1,12],
+[25,12],
+[26,12]
+]
+
+totalHolidays=len(daysOff)+4 # Easter and Pentecost add 2+2 more days - 1 on Sunday, one on Monday for each
+
+for year in range(2016,2030):
+    holidaysInWeekend=2 # by default we start with 2 in weekend (Pentecost and Easter)
+    for dayOff in daysOff:
+        day = dayOff[0]
+        month = dayOff[1]
+        d = datetime.date(year,month,day)
+        weekday = d.weekday()
+        if weekday > 4:
+            # that means 5 or 6
+            # meaning Saturday or Sunday
+            holidaysInWeekend+=1
+
+    print("Year %d has %d (out of %d) holidays falling on weekend." % (year,holidaysInWeekend,totalHolidays))
+
