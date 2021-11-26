fd272454cd01fd54148a200ec43d625cb341af6a
Iulian Serbanoiu <iulian-nicu.serbanoiu@atos.net>
2021-11-26 22:04:25 +0200

    new game: frog

--- 09_pygame/frog.py
+++ 09_pygame/frog.py
@@ -13,36 +13,19 @@ fps_limiter = pygame.time.Clock()
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("intro_ball.gif")
+frog = pygame.image.load("cute-frog.png")
 background = pygame.image.load("background.jpg")
-#ball = pygame.image.load("pp.jfif")
-ballrect = ball.get_rect()
-
-print(str(ballrect))
-
-def increase_speed():
-    global speed
-    if speed < 20:
-        speed += 1
-    print("speed is:"+str(speed))
-
-def decrease_speed():
-    global speed
-    if speed > 1:
-        speed -= 1
-    print("speed is:"+str(speed))
+fw,fh = frog.get_size()
+frogrect = (width/2-fw/2,height-fh-10,fw,fh)
 
 drawLine=False
 done = False
-paused = False
 while not done:
     if pygame.key.get_pressed()[pygame.K_UP]:
-        increase_speed()
-        paused = False
+        pass
 
     if pygame.key.get_pressed()[pygame.K_DOWN]:
-        decrease_speed()
-        paused = False
+        pass
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -56,33 +39,19 @@ while not done:
             if event.key == pygame.K_g:
                 if framerate >= 60:
                     framerate-=30
-            if event.key == pygame.K_SPACE:
-                paused = not paused
         elif event.type == pygame.MOUSEBUTTONDOWN:
             drawLine = True
         elif event.type == pygame.MOUSEBUTTONUP:
             drawLine = False
 
-    if paused == True:
-        speed_sign = [ 0 , 0 ]
-    else:
-        speed_sign = [ speed * xsign, speed * ysign ]
-
-    ballrect = ballrect.move(speed_sign)
-    if ballrect.left < 0 or ballrect.right > width:
-        xsign = -xsign
-    if ballrect.top < 0 or ballrect.bottom > height:
-        ysign = -ysign
-
     screen.fill(black)
     screen.blit(background,(0,0,width,height))
     if drawLine:
-        middlex = (ballrect.left+ballrect.right)/2
-        middley = (ballrect.top+ballrect.bottom)/2
+        middlex = frogrect[0]+frogrect[2]/2
+        middley = frogrect[1]+frogrect[2]/2
         mousepos=pygame.mouse.get_pos()
-        pygame.draw.line(screen,(255,0,0),(middlex,middley),mousepos)
-
-    screen.blit(ball, ballrect)
+        pygame.draw.line(screen,(255,0,0),(middlex,middley),mousepos,3  )
+    screen.blit(frog, frogrect)
     pygame.display.flip()
     fps_limiter.tick(framerate)
 
