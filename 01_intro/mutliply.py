18515dbcdb7ce6daba585817ded57d66539720d6
Iulian Serbanoiu <iulian-nicu.serbanoiu@atos.net>
2022-01-28 22:01:14 +0200

    option to select each multiplication term

--- 01_intro/mutliply.py
+++ 01_intro/mutliply.py
@@ -3,30 +3,41 @@
 import random
 import time
 
-max_num=10
+max_num1=10
+max_num2=10
 
-msg = "Enter maximum multiplication factor or enter for default one (default: 10):"
-try:
-    val = input(msg)
-    max_num=int(val)
-except:
-    pass
+def get_value_from_user(msg1,def_value):
+    msg = msg1 + "(default:" + str(def_value) + ")"
+    max_num=def_value
+    try:
+        val = input(msg)
+        max_num=int(val)
+    except:
+        pass
+    return max_num
 
-print("Maximum value is 10")
+max_num1 = get_value_from_user("Enter maximum value for first term or enter for default one",10)
+max_num2 = get_value_from_user("Enter maximum value for second term or enter for default one",10)
+
 
 start_time=time.perf_counter()
 
 valid=0
 counter=0
 while True:
-    f1 = random.randint(1,max_num)
-    f2 = random.randint(1,max_num)
+    f1 = random.randint(1,max_num1)
+    f2 = random.randint(1,max_num2)
     prefix = '%05d' % (counter+1)
-    msg = prefix + ": " + str(f1)+" x "+str(f2)+" ="
+    mult_str=str(f1)+" x "+str(f2)+" ="
+    msg = prefix + ": " + mult_str
     try:
         val = int(input(msg))
-        if val == f1*f2:
+        expected = f1*f2
+        if val == expected:
             valid+=1
+        else:
+            print("Bad answer. Correct one is:" + mult_str + str(expected))
+        
         counter+=1
     except:
         break
